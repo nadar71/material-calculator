d3e9c9478e5120338dfd2d969d6546a94c1e3a7c
simone <simone.mapelli@gmail.com>
2026-01-08 12:25:46 +0100

    feat: Expression writeer with tests

---
+++ app/src/test/java/eu/indiewalkabout/material_calculator/domain/ExpressionWriterTest.kt
@@ -0,0 +1,52 @@
+package com.plcoding.materialcalculator.domain
+
+import eu.indiewalkabout.material_calculator.domain.CalculatorAction
+import eu.indiewalkabout.material_calculator.domain.Operation
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
+
+
+class ExpressionWriterTest {
+
+    private lateinit var writer: ExpressionWriter
+
+    @Before
+    fun setUp() {
+        writer = ExpressionWriter()
+    }
+
+    @Test
+    fun `Initial parentheses parsed`() {
+        writer.processAction(CalculatorAction.Parentheses)
+        writer.processAction(CalculatorAction.Number(5))
+        writer.processAction(CalculatorAction.Op(Operation.ADD))
+        writer.processAction(CalculatorAction.Number(4))
+        writer.processAction(CalculatorAction.Parentheses)
+
+        // assertThat(writer.expression).isEqualTo("(5+4)")
+        assertThat("(5+4)", `is`(writer.expression))
+    }
+
+    @Test
+    fun `Closing parentheses at the start not parsed`() {
+        writer.processAction(CalculatorAction.Parentheses)
+        writer.processAction(CalculatorAction.Parentheses)
+
+        // assertThat(writer.expression).isEqualTo("((")
+        assertThat("((", `is`(writer.expression))
+
+    }
+
+    @Test
+    fun `Parentheses around a number are parsed`() {
+        writer.processAction(CalculatorAction.Parentheses)
+        writer.processAction(CalculatorAction.Number(6))
+        writer.processAction(CalculatorAction.Parentheses)
+
+        // assertThat(writer.expression).isEqualTo("(6)")
+        assertThat("(6)", `is`(writer.expression))
+
+    }
+}
